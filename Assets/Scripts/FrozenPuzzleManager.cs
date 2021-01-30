29f23b2764faa1ab0b4a599696da34cd8b1521d4
Luca Ruiters <luca.ruiters@hva.nl>
2021-01-30 16:24:07 +0100

    Added Ice over the lever

--- Assets/Scripts/FrozenPuzzleManager.cs
+++ Assets/Scripts/FrozenPuzzleManager.cs
@@ -5,5 +5,26 @@ using UnityEngine;
 public class FrozenPuzzleManager : MonoBehaviour
 {
     [SerializeField] List<AudioSource> speakers;
-    [SerializeField] GameObject Lever;
+    [SerializeField] GameObject ice;
+    bool allActive = false;
+
+    private void Update()
+    {
+        foreach(AudioSource source in speakers)
+        {
+            if (!source.isPlaying || source.volume == 0)
+            {
+                allActive = false;
+                break;
+            } else
+            {
+                allActive = true;
+            }
+        }
+
+        if (allActive)
+        {
+            ice.SetActive(false);
+        }
+    }
 }
