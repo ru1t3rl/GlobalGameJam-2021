dda4214155cea027127bce7c69a9400f1a41a0de
Luca Ruiters <luca.ruiters@hva.nl>
2021-01-31 01:40:12 +0100

    Fixed some merge conflict left overs

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -4,37 +4,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveObject : MonoBehaviour
+namespace Test
 {
-    [SerializeField] LayerMask moveAbleLayer;
-    [SerializeField] float maxDistance = 10;
-    [SerializeField] float tweenTime = 0.5f;
-    RaycastHit hit;
-
-    public void TryMoveObject()
+    public class MoveObject : MonoBehaviour
     {
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance, moveAbleLayer))
-        {
-            if (hit.transform != null)
-            {
-                hit.transform.DOMove(new Vector3(transform.position.x,
-                                                 transform.position.y,
-                                                 transform.position.z),
-                                     tweenTime);
+        [SerializeField] LayerMask moveAbleLayer;
+        [SerializeField] float maxDistance = 10;
+        [SerializeField] float tweenTime = 0.5f;
+        RaycastHit hit;
 
-                this.transform.DOMoveY(hit.transform.localScale.y * 2, tweenTime / 2);
+        public void TryMoveObject()
+        {
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance, moveAbleLayer))
+            {
+                if (hit.transform != null)
+                {
+                    hit.transform.DOMove(new Vector3(transform.position.x,
+                                                     transform.position.y,
+                                                     transform.position.z),
+                                         tweenTime);
+
+                    this.transform.DOMoveY(hit.transform.localScale.y * 2, tweenTime / 2);
+                }
             }
         }
-    }
 
-    public void RemoveFromPlayer(Object Player)
-    {
-        Destroy(this);
-    }
+        public void RemoveFromPlayer(Object Player)
+        {
+            Destroy(this);
+        }
 
-    public void AddToPlayer(Object player)
-    {
-        GameObject gobj = (GameObject)player;
-        gobj.AddComponent<MoveObject>();
+        public void AddToPlayer(Object player)
+        {
+            GameObject gobj = (GameObject)player;
+            gobj.AddComponent<MoveObject>();
+        }
     }
 }
